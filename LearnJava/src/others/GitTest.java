f491d50a22f47ad5df782c7089e72e570018fb8e
sandeep <sandeep@LAPTOP-C1873E79>
2018-07-19 22:08:11 +0530

    pushing additonal changes

---
+++ LearnJava/src/others/GitTest.java
@@ -0,0 +1,12 @@
+package others;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is just to test Git");
+
+	}
+
+}
